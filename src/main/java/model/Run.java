171d062f5766b2278b544cccd862b2e77426b364
cjm864 <gary.bannerman@motorolasolutions.com>
2019-08-26 15:55:07 +0100

    Split out ability to get the JSON data into methods.

--- src/main/java/model/Run.java
+++ src/main/java/model/Run.java
@@ -24,9 +24,24 @@ public class Run {
 
     public static void main(String[] args) throws IOException {
         Run run = new Run();
+        List<Footballer> footballers = run.getFootballersInSquad();
+        run.setFootballerNameAndTeamId(footballers);
+        run.setTeamName(footballers);
+        run.setFixtureDifficultyRating(footballers);
 
-        // Get a list of currently selected players
-        JSONObject picksJson = run.getJsonObject(JsonUrl.PICKS_PREFIX.url + CURRENT_WEEK + JsonUrl.PICKS_SUFFIX.url);
+        for (Footballer footballer : footballers) {
+            System.out.println("Player: " + footballer.getWebName() +
+                    "| Opposition: " + footballer.getOppositionList().toString() +
+                    "| Total: " + footballer.getDifficultyTotal());
+        }
+    }
+
+    private JSONObject getJsonObject(String url) throws IOException {
+        return new JSONObject(IOUtils.toString(new URL(url), Charset.forName("UTF-8")));
+    }
+
+    private List<Footballer> getFootballersInSquad() throws IOException {
+        JSONObject picksJson = getJsonObject(JsonUrl.PICKS_PREFIX.url + CURRENT_WEEK + JsonUrl.PICKS_SUFFIX.url);
         JSONArray picksArray = picksJson.getJSONArray("picks");
         Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Pick> picksAdapter = moshi.adapter(Pick.class);
@@ -41,10 +56,13 @@ public class Run {
                 footballers.add(footballer);
             }
         }
+        return footballers;
+    }
 
-        // For the selected player ids get the footballer name and team id
-        JSONObject elementsJson = run.getJsonObject(JsonUrl.STATIC.url);
+    private void setFootballerNameAndTeamId(List<Footballer> footballers) throws IOException {
+        JSONObject elementsJson = getJsonObject(JsonUrl.STATIC.url);
         JSONArray elementsArray = elementsJson.getJSONArray("elements");
+        Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Element> elementsAdapter = moshi.adapter(Element.class);
 
         for (int j = 0; j < elementsArray.length(); j++) {
@@ -56,9 +74,12 @@ public class Run {
                 }
             }
         }
+    }
 
-        // Get the text value for the team id
+    private void setTeamName(List<Footballer> footballers) throws IOException {
+        JSONObject elementsJson = getJsonObject(JsonUrl.STATIC.url);
         JSONArray teamsArray = elementsJson.getJSONArray("teams");
+        Moshi moshi = new Moshi.Builder().build();
         JsonAdapter<Team> teamsAdapter = moshi.adapter(Team.class);
         for (int i = 0; i < teamsArray.length(); i++) {
             Team team = teamsAdapter.fromJson(teamsArray.get(i).toString());
@@ -68,76 +89,60 @@ public class Run {
                 }
             }
         }
+    }
 
+    private void setFixtureDifficultyRating(List<Footballer> footballers) throws IOException {
         //  Get the fixtures for each
         for (int weekCounter = 1; weekCounter < WEEKS_TO_EVALUATE; weekCounter++) {
             JSONArray fixturesArray = new JSONArray(IOUtils.toString(
                     new URL(JsonUrl.FIXTURES.url + (CURRENT_WEEK + weekCounter)), Charset.forName("UTF-8")));
+            Moshi moshi = new Moshi.Builder().build();
             JsonAdapter<Fixture> fixturesAdapter = moshi.adapter(Fixture.class);
+
             for (int i = 0; i < fixturesArray.length(); i++) {
                 Fixture fixture = fixturesAdapter.fromJson(fixturesArray.get(i).toString());
                 for (Footballer footballer : footballers) {
-
                     assert fixture != null;
                     boolean isFootballerPartOfAwayTeam = (fixture.team_a == footballer.getTeamId());
                     boolean isFootballerPartOfHomeTeam = (fixture.team_h == footballer.getTeamId());
 
                     if (isFootballerPartOfAwayTeam) {
-                        int oppositionId = fixture.team_h;
-                        Opposition opposition = new Opposition();
-                        opposition.setTeamId(oppositionId);
-                        opposition.setDifficultyRating(fixture.team_a_difficulty);
-
-                        //  Translate the team id to text
-                        for (int j = 0; j < teamsArray.length(); j++) {
-                            Team team = teamsAdapter.fromJson(teamsArray.get(j).toString());
-                            if (team != null && team.id == oppositionId) {
-                                opposition.setName(team.name);
-                                break;
-                            }
-                        }
-
-                        List<Opposition> oppositionList = footballer.getOppositionList();
-                        oppositionList.add(opposition);
-                        footballer.setOppositionList(oppositionList);
-                        int currentDifficultyScore = footballer.getDifficultyTotal();
-                        footballer.setDifficultyTotal(currentDifficultyScore + opposition.getDifficultyRating());
-
-
+                        setOppositionNameAndDifficulty(footballer, fixture, false);
                     } else if (isFootballerPartOfHomeTeam) {
-                        System.out.println("is in Home team");
-                        int oppositionId = fixture.team_a;
-                        Opposition opposition = new Opposition();
-                        opposition.setTeamId(oppositionId);
-                        opposition.setDifficultyRating(fixture.team_h_difficulty);
-                        System.out.println("difficulty rating: " + fixture.team_a_difficulty);
-
-                        //  Translate the team id to text
-                        for (int j = 0; j < teamsArray.length(); j++) {
-                            Team team = teamsAdapter.fromJson(teamsArray.get(j).toString());
-                            if (team != null && team.id == oppositionId) {
-                                opposition.setName(team.name);
-                            }
-                        }
-
-                        List<Opposition> oppositionList = footballer.getOppositionList();
-                        oppositionList.add(opposition);
-                        footballer.setOppositionList(oppositionList);
-                        int currentDifficultyScore = footballer.getDifficultyTotal();
-                        footballer.setDifficultyTotal(currentDifficultyScore + opposition.getDifficultyRating());
+                        setOppositionNameAndDifficulty(footballer, fixture, true);
                     }
                 }
             }
         }
+    }
 
-        for (Footballer footballer : footballers) {
-            System.out.println("Player: " + footballer.getWebName() + " | Opposition: " + footballer.getOppositionList().toString()
-                    + "| Total: " + footballer.getDifficultyTotal());
+    private void setOppositionNameAndDifficulty(Footballer footballer, Fixture fixture, boolean isFootballerAtHome) throws IOException {
+        Opposition opposition = new Opposition();
+        if (isFootballerAtHome) {
+            opposition.setTeamId(fixture.team_a);
+            opposition.setDifficultyRating(fixture.team_h_difficulty);
+        } else {
+            opposition.setTeamId(fixture.team_h);
+            opposition.setDifficultyRating(fixture.team_a_difficulty);
         }
 
+        //  Translate the team id to text
+        JSONObject elementsJson = getJsonObject(JsonUrl.STATIC.url);
+        JSONArray teamsArray = elementsJson.getJSONArray("teams");
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Team> teamsAdapter = moshi.adapter(Team.class);
+        for (int j = 0; j < teamsArray.length(); j++) {
+            Team team = teamsAdapter.fromJson(teamsArray.get(j).toString());
+            if (team != null && team.id == opposition.getTeamId()) {
+                opposition.setName(team.name);
+            }
+        }
+
+        List<Opposition> oppositionList = footballer.getOppositionList();
+        oppositionList.add(opposition);
+        footballer.setOppositionList(oppositionList);
+        int currentDifficultyScore = footballer.getDifficultyTotal();
+        footballer.setDifficultyTotal(currentDifficultyScore + opposition.getDifficultyRating());
     }
 
-    private JSONObject getJsonObject(String url) throws IOException {
-        return new JSONObject(IOUtils.toString(new URL(url), Charset.forName("UTF-8")));
-    }
 }
